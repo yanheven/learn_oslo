cc9f473824c2f2d5cd0be0582d6fbeba6e32eeea
yan.haifeng <yanheven@qq.com>
2016-05-10 09:34:19 +0800

    add oslo.service examples

--- learn_oslo/cmd/demo.py
+++ learn_oslo/cmd/demo.py
@@ -1,7 +1,9 @@
 __author__ = 'evan'
 import sys
-from learn_oslo.cmd.api import main
+from learn_oslo.cmd.api import main as api_main
+from learn_oslo.cmd.api_service import main as service_main
 
 
 if __name__ == '__main__':
-    sys.exit(main())
+    # sys.exit(api_main())
+    sys.exit(service_main())
